b29d8978ac840ad5c467ef8becb2d2d50abb8fbd
Ken Cruz <ken@kencruz.ca>
2021-11-03 22:42:51 -0400

    fixed tests

--- test/poppinBottlesTest.js
+++ test/poppinBottlesTest.js
@@ -3,15 +3,15 @@ const assert = require("chai").assert;
 
 describe("#poppinBottles", () => {
   it("should return 15 from 10", () => {
-    assert.strictEqual(poppinBottles(10), 15);
+    assert.strictEqual(poppinBottles(10).bottles, 15);
   });
   it("should return 35 from 20", () => {
-    assert.strictEqual(poppinBottles(10), 15);
+    assert.strictEqual(poppinBottles(20).bottles, 35);
   });
   it("should return 55 from 30", () => {
-    assert.strictEqual(poppinBottles(10), 15);
+    assert.strictEqual(poppinBottles(30).bottles, 55);
   });
   it("should return 75 from 40", () => {
-    assert.strictEqual(poppinBottles(10), 15);
+    assert.strictEqual(poppinBottles(40).bottles, 75);
   });
 });
